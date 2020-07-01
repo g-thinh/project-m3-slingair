6ab94ad47b9710a9fbf2ceb4f34af924f4242705
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-07-01 13:53:49 -0400

    endpoint to fetch seat availability done

--- handlers.js
+++ handlers.js
@@ -1,11 +1,21 @@
 const { flights } = require("./test-data/flightSeating");
 
+//this function will return all of the flight's seating data
+//if the flight exists
 const handleFlight = (req, res) => {
   const { flightNumber } = req.params;
   // get all flight numbers
   const allFlights = Object.keys(flights);
   // is flightNumber in the array?
   console.log("REAL FLIGHT: ", allFlights.includes(flightNumber));
+
+  if(allFlights.includes(flightNumber)) {
+    //console.log(flights[flightNumber]);
+    //send all of the flight's data
+    res.status(200).send(flights[flightNumber]);
+  } else {
+    res.status(400).send({message: "Flight not Found"});
+  }
 };
 
 const handleSeats = (req, res) => {
