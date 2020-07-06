44113112f14b62d75bcd9b177c812bfe31832141
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-07-05 21:08:36 -0400

    added comments

--- public/seat-select/js/view-reservation.js
+++ public/seat-select/js/view-reservation.js
@@ -2,25 +2,39 @@ const inputs = document.querySelectorAll('input');
 const form = document.querySelector('form');
 //console.log(inputs);
 
+//this function will validate the form and redirect the user to their
+//confirmation page based on their reservation id#
 const getReservation = (event) => {
+
+  //as this is a custom form validation, prevent default submission from occuring
   event.preventDefault();
-  console.log(event);
+  // console.log(event);
+
+  //the form has 4 input fields, that once combined will give us an UUID string
   let userInput = [];
+
+  //push all values of each input box into the userInput array
   inputs.forEach(input => userInput.push(input.value));
+
+  //...and join them all into a string
   let inputId = userInput.join('-');
-  console.log("User Inputted:", inputId);
-  console.log("Fetching data...");
+
+
+  // console.log("User Inputted:", inputId);
+  // console.log("Fetching data...");
+
+  //now we want to fetch all current reservations to compare with the inputted
+  //id
   fetch('/users', {method: 'GET'})
   .then(res => res.json())
   .then(data => {
-    console.log(data);
-    // debugger;
-    data.forEach(res => console.log(res));
-    //let reservations = Object.keys(data);
-    //console.log(reservations);
+    //data.forEach(res => console.log(res));
+
+    //match to the first ID in the reservations database
     let res = data.find(rsv => rsv.id == inputId);
 
-    //console.log(resId)
+    //if there is a match, then redirect, if its undefined then display
+    //a message.
     if(res) {
       window.location = `/seat-select/confirmed/${res.id}`;
     } else {
@@ -30,5 +44,3 @@ const getReservation = (event) => {
   })
   .catch(err => console.log(err));
 }
-
-//form.addEventListener('submit', getReservation);
